61f6555e050c8d20e49cc31eeb99c7a561c95215
Eldad Assis <eldada@jfrog.com>
2024-11-09 15:49:20 -0700

    [WIP] Set RDS storage type and size

--- 6.artifactory-aws-install/rds.tf
+++ 6.artifactory-aws-install/rds.tf
@@ -10,18 +10,22 @@ resource "aws_db_subnet_group" "artifactory_subnet_group" {
 }
 
 resource "aws_db_instance" "artifactory_db" {
-  identifier            = "artifactory-db"
-  engine               = "postgres"
-  engine_version       = "16.4" # Specify the desired version
-  instance_class       = "db.m7g.large" # Change as needed based on expected load
-  allocated_storage     = 50 # Minimum is 20 GB for PostgreSQL
-  storage_type         = "gp3"
-  username             = var.db_username
-  password             = var.db_password
-  db_name              = "artifactory"
+  identifier             = "artifactory-db"
+  engine                 = "postgres"
+  engine_version         = "16.4" # Specify the desired version
+  instance_class         = "db.m7g.large" # Change as needed based on expected load
+
+  storage_type           = "gp3"        # Using gp3 for storage type
+  allocated_storage      = 200          # Set desired storage size in GB
+  max_allocated_storage  = 400          # Set maximum size for storage autoscaling (optional)
+
+  username               = var.db_username
+  password               = var.db_password
+  db_name                = "artifactory"
+
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
-  db_subnet_group_name = aws_db_subnet_group.artifactory_subnet_group.name
-  skip_final_snapshot  = true
+  db_subnet_group_name   = aws_db_subnet_group.artifactory_subnet_group.name
+  skip_final_snapshot    = true
 
   tags = {
     Name = "artifactory-db"

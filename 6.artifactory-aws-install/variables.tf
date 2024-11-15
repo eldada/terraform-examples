5811e87569ff573f7ca8778f971e06e231b43f51
Eldad Assis <eldada@jfrog.com>
2024-11-14 20:25:40 -0700

    Small adjustment to a comment

--- 6.artifactory-aws-install/variables.tf
+++ 6.artifactory-aws-install/variables.tf
@@ -1,10 +1,10 @@
 # Setup the required variables
 
-# WARNING: CIDR "0.0.0.0/0" is full public access to the cluster, you should use a more restrictive CIDR
 variable "region" {
   default = "eu-central-1"
 }
 
+# WARNING: CIDR "0.0.0.0/0" is full public access to the cluster. You should use a more restrictive CIDR
 variable "cluster_public_access_cidrs" {
   default = "0.0.0.0/0"
 }

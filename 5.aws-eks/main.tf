a626939ea5057ba7d1eef2a3f18f04c9e99e0de3
Eldad Assis <eldada@jfrog.com>
2024-12-12 10:33:46 +0200

    Fix instance type (ARM)

--- 5.aws-eks/main.tf
+++ 5.aws-eks/main.tf
@@ -123,7 +123,7 @@ module "eks" {
         one = {
             name = "node-group-1"
 
-            instance_types = ["t3.small"]
+            instance_types = ["t4g.small"]
 
             min_size     = 1
             max_size     = 3

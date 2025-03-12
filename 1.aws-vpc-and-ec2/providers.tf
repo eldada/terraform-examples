ce32755c26f3ab24fca5dc857c27fb2517d60b2f
Eldad Assis <eldada@jfrog.com>
2025-03-12 16:31:29 +0200

    Use a local backend in a "state" directory

--- 1.aws-vpc-and-ec2/providers.tf
+++ 1.aws-vpc-and-ec2/providers.tf
@@ -1,3 +1,28 @@
+terraform {
+  # Use a local backend
+  backend "local" {
+    path = "./state/terraform.tfstate"
+  }
+
+  ## Configure the remote backend (Artifactory)
+  ## This will store the state file in Artifactory.
+  ## Follow https://jfrog.com/help/r/jfrog-artifactory-documentation/terraform-backend-repository
+  # backend "remote" {
+  #   hostname = "eldada.jfrog.io"
+  #   organization = "terraform-backend"
+  #   workspaces {
+  #     prefix = "demo-"
+  #   }
+  # }
+
+  required_providers {
+    # AWS provider
+    aws = {
+      source  = "hashicorp/aws"
+    }
+  }
+}
+
 # Configure the AWS Provider and set the region
 provider "aws" {
   region = var.region
